68aedde5480c29674ed8dc8d7734d4023ea63701
lai-chun-lin <axd5201211@gmail.com>
2019-12-04 12:00:03 +0800

    加入刪除的func

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -35,4 +35,11 @@ class TaskController extends Controller
         return redirect('/tasks');
     }
 
+    public function destroy(Request $request, Task $task)
+    {
+        $this->authorize('destroy', $task);
+        $task->delete();
+        return redirect('/tasks');
+    }
+
 }
